983e017bd1da2b29642fc9bafc27118efb90729c
Raul Gomes Reis <115173345+raulreis123@users.noreply.github.com>
2024-03-18 15:23:15 -0300

    Adicionando INSERTS no banco de dados.

---
+++ BancoDeDados/testebdTransControl.sql
@@ -0,0 +1,20 @@
+INSERT INTO Cadastro VALUES
+(default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', 'R. Boa Vista, 150 - Centro Histórico de São Paulo, São Paulo', '58518069000191', '08007240555', '12345678'),
+(default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', 'Avenida Sargento Jose Siqueira, 427 - Jardim Paraiso, Barueri - SP', '48748230000160', '11 41992730', '12345678'),
+(default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', 'Av. Valter Boveri, 501A CEP: 06053-120 Jardim Novo Osasco - SP', '45645462000366', '35924261', '12345678');
+
+INSERT INTO ModeloOnibus VALUES
+(default, 3, 120, 'JPV8440'),
+(default, 3, 120, 'JPV8440'),
+(default, 3, 120, 'JPV8440');
+
+INSERT INTO sensor VALUES
+(default, 'ENTRADA'),
+(default, 'SAIDA')
+(default, 'SAIDA');
+
+INSERT INTO dados VALUES
+(default, 1, '2024-03-09 14:30:28'),
+(default, 0, '2024-03-09 13:14:58'),
+(default, 1, '2024-03-09 12:46:10');
+
