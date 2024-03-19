12906df935be418fdf09918b813e309a82dca372
Raul Reis <raulgomesreis@gmail.com>
2024-03-18 22:21:25 -0300

    Inserção de dados

--- BancoDeDados/TransControl.sql
+++ BancoDeDados/TransControl.sql
@@ -1,13 +1,10 @@
 -- TABELAS SPRINT 1
-
 -- CRIAÇÃO DO BANCO DE DADOS
 CREATE DATABASE TransControl;
-
-
+USE TransControl;
 
 
 -- TABELA 1 (CADASTRO)
-USE TransControl;
 
 CREATE TABLE Cadastro (
 idEmpresa int primary key auto_increment,
@@ -20,24 +17,34 @@ telefone char(14) NOT NULL,
 senha varchar(20) NOT NULL
 );
 
+-- Inserção de dados na tabela Cadastro
+INSERT INTO Cadastro VALUES
+    (default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', 'R. Boa Vista, 150 - Centro Histórico de São Paulo, São Paulo', '58518069000191', '08007240555', '12345678'),
+    (default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', 'Avenida Sargento Jose Siqueira, 427 - Jardim Paraiso, Barueri - SP', '48748230000160', '41992730', '12345678'),
+    (default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', 'Av. Valter Boveri, 501A CEP: 06053-120 Jardim Novo Osasco - SP', '45645462000366', '35924261', '12345678');
+
 DESCRIBE Cadastro;
 SELECT * FROM Cadastro;
 
 
 
 
-
--- TABELA 2 (MODELO DO ONIBUS)
-USE TransControl;
+--	TABELA 2 (MODELO DO ONIBUS)
 
 CREATE TABLE ModeloOnibus(
 	idOnibus int primary key auto_increment NOT NULL,
 	qtdPortas int NOT NULL,
-    	capacidade int NOT NULL,
-    	placa char(7),
-	peso int,
+    capacidade int NOT NULL,
+	placa char(7),
+	peso int, -- Em KG
     CONSTRAINT chkPortas CHECK(qtdPortas >= 2 AND qtdPortas <= 8)
 );				
+
+-- Inserção de dados na tabela ModeloOnibus
+INSERT INTO ModeloOnibus VALUES
+    (default, 3, 120, 'JPV8440', 26000), -- Onibus Articulado
+    (default, 3, 20, 'BRA5679', 4000), -- Micro-Onibus
+    (default, 3, 70, 'JTW9010', 16000); -- Onibus convencional
 
 DESCRIBE ModeloOnibus;
 SELECT * FROM ModeloOnibus;
@@ -45,9 +52,7 @@ SELECT * FROM ModeloOnibus;
 
 
 
-
 -- TABELA 3 (SENSORES)
-USE TransControl;
 
 CREATE TABLE sensor(
 	idSensor int primary key auto_increment,
@@ -55,21 +60,32 @@ CREATE TABLE sensor(
      CONSTRAINT chkPorta CHECK(portaSensor = 'SAIDA' OR portaSensor = 'ENTRADA')
 );
 
-DESCRIBE Sensor;
-SELECT * FROM Sensor;
+-- Inserção de dados na tabela sensor
+INSERT INTO sensor VALUES
+    (default, 'ENTRADA'),
+    (default, 'SAIDA'),
+    (default, 'SAIDA');
+
+DESCRIBE sensor;
+SELECT * FROM sensor;
 
 
 
 
 -- TABELA 4 (DADOS)
-USE TransControl;
 
 CREATE TABLE dados(
 	idDado int primary key auto_increment,
     ativacao tinyint,
-	horarioAtivacao date,
+	horarioAtivacao datetime DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT chkDado CHECK(ativacao = 0 OR ativacao = 1)
     );
 
-DESCRIBE dadoSensor;
-SELECT * FROM dadoSensor;
+-- Inserção de dados na tabela dados
+INSERT INTO dados VALUES
+    (default, 1, default),
+    (default, 0, default),
+    (default, 1, default);
+
+DESCRIBE dados;
+SELECT * FROM dados;
--- BancoDeDados/testebdTransControl.sql
+++ BancoDeDados/testebdTransControl.sql
@@ -1,20 +1,20 @@
 INSERT INTO Cadastro VALUES
-(default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', 'R. Boa Vista, 150 - Centro Histórico de São Paulo, São Paulo', '58518069000191', '08007240555', '12345678'),
-(default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', 'Avenida Sargento Jose Siqueira, 427 - Jardim Paraiso, Barueri - SP', '48748230000160', '11 41992730', '12345678'),
-(default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', 'Av. Valter Boveri, 501A CEP: 06053-120 Jardim Novo Osasco - SP', '45645462000366', '35924261', '12345678');
+    (default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', 'R. Boa Vista, 150 - Centro Histórico de São Paulo, São Paulo', '58518069000191', '08007240555', '12345678'),
+    (default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', 'Avenida Sargento Jose Siqueira, 427 - Jardim Paraiso, Barueri - SP', '48748230000160', '11 41992730', '12345678'),
+    (default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', 'Av. Valter Boveri, 501A CEP: 06053-120 Jardim Novo Osasco - SP', '45645462000366', '35924261', '12345678');
 
 INSERT INTO ModeloOnibus VALUES
-(default, 3, 120, 'JPV8440'),
-(default, 3, 120, 'JPV8440'),
-(default, 3, 120, 'JPV8440');
+    (default, 3, 120, 'JPV8440'), --Onibus Articulado
+    (default, 3, 20, 'JPV8440'), --Micro-Onibus
+    (default, 3, 70, 'JPV8440'); --Onibus convencional
 
 INSERT INTO sensor VALUES
-(default, 'ENTRADA'),
-(default, 'SAIDA')
-(default, 'SAIDA');
+    (default, 'ENTRADA'),
+    (default, 'SAIDA')
+    (default, 'SAIDA');
 
 INSERT INTO dados VALUES
-(default, 1, '2024-03-09 14:30:28'),
-(default, 0, '2024-03-09 13:14:58'),
-(default, 1, '2024-03-09 12:46:10');
+    (default, 1, '2024-03-09 14:30:28'),
+    (default, 0, '2024-03-09 13:14:58'),
+    (default, 1, '2024-03-09 12:46:10');
 
