3e23d46aab32bdfd62b6c39fdfce30976bd7fa9b
anamoraeszx <ana.moraes@sptech.school>
2024-04-24 11:55:22 -0300

    Script Banco de Dados

--- BancoDeDados/BancoDeDadosTransControl.sql
+++ BancoDeDados/BancoDeDadosTransControl.sql
@@ -2,55 +2,9 @@ CREATE DATABASE TransControl;
 USE TransControl;
 
 
--- TABELA 1 (EMPRESA)
+-- TABELA 1 (LINHA DO ONIBUS)
 
-CREATE TABLE Empresa (
-idEmpresa int primary key auto_increment,
-razaoSocial varchar(255) NOT NULL,
-nomeFantasia varchar(255),
-email varchar(40) NOT NULL,
-logradouro varchar(150) NOT NULL,
-CNPJ char(14) unique NOT NULL,
-telefone char(14) NOT NULL,
-senha varchar(20) NOT NULL,
-chaveUnica int unique not null
-);
-
--- Inserção de dados na tabela Empresa
-INSERT INTO Empresa VALUES
-    (default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', 'R. Boa Vista, 150 - Centro Histórico de São Paulo, São Paulo', 58518069000191, 08007240555, 12345678, 123),
-    (default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', 'Avenida Sargento Jose Siqueira, 427 - Jardim Paraiso, Barueri - SP', '48748230000160', 41992730, 12345678, 321),
-    (default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', 'Av. Valter Boveri, 501A CEP: 06053-120 Jardim Novo Osasco - SP', 45645462000366, 35924261, 12345678, 456);
-
-DESCRIBE Empresa;
-SELECT * FROM Empresa;
-
--- TABELA 2 (FUNCIONARIO)
-
-create table funcionario(
-idFuncionario int primary key auto_increment,
-nome varchar (45) not null,
-email varchar(45) unique not null,
-senha varchar(20) not null,
-fkEmpresa int,
-constraint fkEmpresaFunc foreign key (fkEmpresa) references Empresa(chaveUnica)
-);
-
-insert into funcionario values
-(default, 'Luiz André Martins', 'luizandre@gmail.com', '989210', 123),
-(default, 'luana Maria Moraes', 'luanamoraes@gmail.com', 'laranja123', 321),
-(default, 'José Mario da Silva', 'josémario@gmail.com', '251922', 456);
-
-DESCRIBE funcionario;
-SELECT * FROM funcionario;
-
-select * from Empresa join funcionario on fkEmpresa = chaveUnica;
-
-
-
--- TABELA 3 (LINHA ONIBUS)
-
-create table linhaOnibus(
+CREATE TABLE linhaOnibus(
 idLinha int primary key auto_increment,
 nome varchar (45),
 destinoInicial varchar (45),
@@ -59,79 +13,167 @@ destinoFinal varchar (45)
 
 insert into linhaOnibus values
 (default, 'B19', 'Bairro Campestre', 'Terminal Santo André'),
-(default, 'I02', 'Centreville', 'Terminal Ana Maria');
+(default, 'I02', 'Centreville', 'Terminal Ana Maria'),
+(default, '285', 'Terminal Santo André', 'Terminal Sao Mateus');
 
 DESCRIBE linhaOnibus;
 SELECT * FROM linhaOnibus;
 
-CREATE TABLE ModeloOnibus(
+-- TABELA 2 ( endereço )
+
+CREATE TABLE endereco (
+idEndereco int primary key auto_increment,
+cep char (9),
+logradouro varchar (60),
+bairro varchar(45),
+numero varchar (12)
+);
+
+insert into endereco values
+(default, '05220-431', 'Vitoria Regia', 'Consolação', '980'),
+(default, '04427-433', 'Alagoas', 'Brigadeiro', '1423');
+
+DESCRIBE endereco;
+SELECT * FROM endereco;
+
+-- TABELA 3 (Empresa)
+
+CREATE TABLE Empresa (
+idEmpresa int primary key auto_increment,
+razaoSocial varchar(255) NOT NULL,
+nomeFantasia varchar(255),
+email varchar(40) NOT NULL,
+CNPJ char(14) unique NOT NULL,
+telefone char(14) NOT NULL,
+senha varchar(20) NOT NULL,
+fkEnderecoEmpresa int,
+CONSTRAINT fkEnderecoEmpresa foreign key (fkEnderecoEmpresa) references endereco(idEndereco)
+);
+
+INSERT INTO Empresa VALUES
+    (default, 'Empresa Metropolitana de Transportes Urbanos de São Paulo', 'EMTU', 'imprensa@emtu.sp.gov.br', '58518069000191', '08007240555', '12345678', 1),
+    (default, 'BB Transporte e Turismo', 'BBTT', 'turismo@benficabbtt.com.br', '48748230000160', '41992730', '12345678', 2),
+    (default, 'Viacao Osasco Ltda', 'Viacao Osasco', 'viacao@osaco.com.br', '45645462000366', '35924261', '12345678', 1);
+
+DESCRIBE Empresa;
+SELECT * FROM Empresa;
+
+-- TABELA 4 (FUNCIONARIO)
+
+create table funcionario(
+idFuncionario int primary key auto_increment,
+nome varchar (45) not null,
+email varchar(45) unique not null,
+senha varchar(20) not null,
+fkEmpresa int,
+CONSTRAINT fkEmpresa foreign key (fkEmpresa) references Empresa(idEmpresa)
+);
+
+insert into funcionario values
+(default, 'Luiz André Martins', 'luizandre@gmail.com', '989210', 13),
+(default, 'luana Maria Moraes', 'luanamoraes@gmail.com', 'laranja123', 14),
+(default, 'José Mario da Silva', 'josémario@gmail.com', '251922', 15);
+
+DESCRIBE funcionario;
+SELECT * FROM funcionario;
+
+-- TABELA 5 (ONIBUS)
+
+CREATE TABLE Onibus(
 	idOnibus int primary key auto_increment NOT NULL,
 	qtdPortas int NOT NULL,
+    CONSTRAINT chkPortas CHECK(qtdPortas >= 2 AND qtdPortas <= 8),
     capacidade int NOT NULL,
+    modeloOnibus varchar (45),
 	placa char(7),
 	peso int, -- Em KG
     fkLinha int,
-    constraint fklinhaOnibus foreign key (fkLinha) references linhaOnibus(idLinha),
-    CONSTRAINT chkPortas CHECK(qtdPortas >= 2 AND qtdPortas <= 8)
+    CONSTRAINT fklinha foreign key (fkLinha) references linhaOnibus(idLinha)
 );
 
-insert into ModeloOnibus values
-(default, 3, 60, 'BD64SB7', '300', '1' ),
+INSERT INTO Onibus VALUES
+    (default, 3, 120, 'Onibus Articulado ','JPV8440', 26000, 1 ),
+    (default, 2, 20, 'Micro-Onibus' ,'BRA5679', 4000, 2), 
+    (default, 3, 70, 'Onibus convencional','JTW9010', 16000, 3); 
+    
+DESCRIBE Onibus;
+SELECT * FROM Onibus;
 
-create table linhaOnibus(
-idLinha int primary key auto_increment,
-nome varchar (45),
-destinoInicial varchar (45),
-destinoFinal varchar (45)
-);	
+-- TABELA 6 (HISTORICO)
 
--- Inserção de dados na tabela ModeloOnibus
-INSERT INTO ModeloOnibus VALUES
-    (default, 3, 120, 'JPV8440', 26000), -- Onibus Articulado
-    (default, 3, 20, 'BRA5679', 4000), -- Micro-Onibus
-    (default, 3, 70, 'JTW9010', 16000); -- Onibus convencional
+create table historico (
+idHistorico int primary key auto_increment,
+dtHora datetime, 
+linhaDestino varchar (45),
+fkOnibusHistorico int,
+CONSTRAINT fkOnibusHistorico foreign key (fkOnibusHistorico) references Onibus (idOnibus)
+);
 
-DESCRIBE ModeloOnibus;
-SELECT * FROM ModeloOnibus;
+insert into historico values
+(default, '2024-03-24 18:02:45', 'B19', 1),
+(default, '2024-03-24 07:30:09', 'I02', 2),
+(default, '2024-03-24 12:40:57', '285', 3);
 
--- TABELA 3 (SENSORES)
+DESCRIBE historico;
+SELECT * FROM historico;
+
+-- TABELA 7 (ALERTA)
+
+create table Alerta (
+idAlerta int primary key auto_increment,
+dtHora datetime,
+tipo varchar (50),
+CONSTRAINT chktipo CHECK(tipo in("Vazio", "Lotação")),
+lotacao int,
+fkOnibusAlerta int,
+CONSTRAINT fkOnibusAlerta foreign key (fkOnibusAlerta) references Onibus(idOnibus)
+);
+
+insert into Alerta values
+(default, '2024-03-24 18:02:45', 'Vazio' ,102, 1),
+(default, '2024-03-24 07:30:09', 'Lotação', 511,2),
+(default, '2024-03-24 12:40:57', 'Lotação',654, 3);
+
+DESCRIBE Alerta;
+SELECT * FROM Alerta;
+
+-- TABELA 8 (8)
 
 CREATE TABLE Sensor(
 	idSensor int primary key auto_increment,
 	portaSensor varchar(7) NOT NULL,
-    fkModeloOnibus int,
-     CONSTRAINT chkPorta CHECK(portaSensor = 'SAIDA' OR portaSensor = 'ENTRADA'),
-     CONSTRAINT fkModeloOnibus foreign key (fkModeloOnibus) references ModeloOnibus(idOnibus)
+    CONSTRAINT chkPorta CHECK(portaSensor = 'SAIDA' OR portaSensor = 'ENTRADA'),
+    fkOnibusSensor int,
+	CONSTRAINT fkOnibusSensor foreign key (fkOnibusSensor) references Onibus(idOnibus)
 );
 
--- Inserção de dados na tabela sensor
 INSERT INTO Sensor VALUES
     (default, 'ENTRADA',1),
-    (default, 'SAIDA',1),
-    (default, 'SAIDA',1);
+    (default, 'SAIDA',2),
+    (default, 'SAIDA',3);
     
-select * from ModeloOnibus join Sensor
-	on idOnibus = fkModeloOnibus;
 
-DESCRIBE sensor;
+DESCRIBE Sensor;
 SELECT * FROM Sensor;
 
-
-
--- TABELA 4 (DADOS)
+-- TABELA 9 (DADOS)
 
 CREATE TABLE dados(
 	idDado int primary key auto_increment,
     ativacao tinyint,
 	horarioAtivacao datetime DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT chkDado CHECK(ativacao = 0 OR ativacao = 1)
+    CONSTRAINT chkDado CHECK(ativacao = 0 OR ativacao = 1),
+    fkSensor int,
+    CONSTRAINT fkSensor foreign key (fkSensor) references Sensor(idSensor)
     );
 
--- Inserção de dados na tabela dados
 INSERT INTO dados VALUES
-    (default, 1, default),
-    (default, 0, default),
-    (default, 1, default);
+    (default, 1, default,1),
+    (default, 0, default,2),
+    (default, 1, default,3);
 
 DESCRIBE dados;
 SELECT * FROM dados;
+
+-- RELACIONAMENTO DAS TABELAS 
+
