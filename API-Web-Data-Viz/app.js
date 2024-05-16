450d496e0f823eb5601b5930650bd39527acc4a1
ArthurRamoss <arthur.santos@sptech.school>
2024-05-16 13:03:05 -0300

    [Ajuste] Tela index, login e cadastro

--- API-Web-Data-Viz/app.js
+++ API-Web-Data-Viz/app.js
@@ -37,14 +37,16 @@ app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
+    88888888888                                 .d8888b.                    888                    888 
+        888                                    d88P  Y88b                   888                    888 
+        888                                    888    888                   888                    888 
+        888  888d888 8888b.  88888b.  .d8888b  888         .d88b.  88888b.  888888 888d888 .d88b.  888 
+        888  888P"      "88b 888 "88b 88K      888        d88""88b 888 "88b 888    888P"  d88""88b 888 
+        888  888    .d888888 888  888 "Y8888b. 888    888 888  888 888  888 888    888    888  888 888 
+        888  888    888  888 888  888      X88 Y88b  d88P Y88..88P 888  888 Y88b.  888    Y88..88P 888 
+        888  888    "Y888888 888  888  88888P'  "Y8888P"   "Y88P"  888  888  "Y888 888     "Y88P"  888 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                                                                                               
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
