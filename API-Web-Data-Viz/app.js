d4b3b91d9a85a83de209ac3db9ee2813281f7d07
ArthurRamoss <arthur.santos@sptech.school>
2024-05-23 14:22:03 -0300

    [Melhoria] Grafico - Rotas

--- API-Web-Data-Viz/app.js
+++ API-Web-Data-Viz/app.js
@@ -17,10 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
+var graficoRouter = require("./src/routes/graficos");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,10 +27,8 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
+app.use("/graficos", graficoRouter);
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
